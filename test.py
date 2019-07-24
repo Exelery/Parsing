01ae6a5e8dbc6f2d6f62f5b34140f2956e8fa82f
Exelery <47086897+Exelery@users.noreply.github.com>
2019-07-24 16:48:37 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-with open("proxies.txt", 'r') as f:
-    answer = list(f.read().split())
-    proxies = {}
-   # for i in answer:
-   #     key = 'https' if 'https' in i else 'http'
-   #     print(key)
-    #    proxies[key] = i
-print(proxies)

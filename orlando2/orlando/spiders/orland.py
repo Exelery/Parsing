d02deeb32ef01d11b93e49ad68c60b8af2a8f417
Exelery <47086897+Exelery@users.noreply.github.com>
2019-08-26 23:15:03 +0300

    clear code

---
+++ orlando2/orlando/spiders/orland.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import scrapy
+#from ..items import OrlandoItem
+import json
+from ..items import OrlandoItem
+from unicodedata import normalize
+
+
+#items = {}
+
+class OrlandSpider(scrapy.Spider):
+    #items = {}
+    result = None
+    name = 'orland'
+    allowed_domains = ['orlando-labs.com']
+    start_urls = ['https://orlando-labs.com/ru']
+
+    def parse(self, response):
+        items = OrlandoItem()
+        numbers = response.css("div.col-sm-3.col-md-3.col-lg-3")
+        for space in numbers:
+            title = normalize("NFKD", " ".join(space.css("h4::text").extract()))
+            items[title] = space.css("span.counter::text").get()
+        items['Команда'] = {}
+        for member in response.css("div.col-md-4.col-sm-4.col-xs-12"):
+            name = member.css("h4::text").get()
+            items['Команда'][name] = {}
+            items['Команда'][name]["Фото"] = response.urljoin(member.css("img::attr(src)").get())
+            items['Команда'][name]["описание"] = " ".join(member.css("h6::text").extract())
+            
+        #result = json.dumps(items, ensure_ascii=False)
+        #print(result)
+        return items                                                                                    
+        
+
+        
+ 
+    
+            
+        
+            
